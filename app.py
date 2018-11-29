de142d0d91f8853183c5320d260d7b7e07f99d86
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-11-29 19:41:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,10 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-
+	
+def reply(text):
+    if text == "hi" :
+	    return TextSendMessage(text="你好")
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))

a6dbd1c3f38214c390a56dca82e7bad3df0eacd6
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-06 19:52:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def Button(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-	    Button(event)
-	    #Reply(event)
+	    #Button(event)
+	    Reply(event)
     except Exception as e:
 	    line_bot_api.reply_message(event.reply_token,
 		    TextSendMessage(text=str(e)))

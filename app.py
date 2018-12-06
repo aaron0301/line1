a91aa7e6903c6dfbf1dc4795a067807bc1a73bf8
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-06 19:50:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def Button(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-	    Reply(event)
+	    Button(event)
+		Reply(event)
     except Exception as e:
 	    line_bot_api.reply_message(event.reply_token,
 		    TextSendMessage(text=str(e)))

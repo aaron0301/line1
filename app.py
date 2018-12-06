2b3e6b3a7de7a043b88ee6608485a93014d5d6b7
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-06 19:39:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,6 +43,32 @@ def Reply(event):
     else:
 	    line_bot_api.reply_message(event.reply_token,
            TextSendMessage(text = event.message.text))
+def Button(event):
+    message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='Menu',
+        text='Please select',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            ),
+            URITemplateAction(
+                label='uri',
+                uri='http://example.com/'
+            )
+        ]
+    )
+)
+line_bot_api.reply_message(event.reply_token, message)
+
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)

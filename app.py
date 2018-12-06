8736f8552dddf0a7a9501e704f3efd34b698faff
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-06 19:07:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def KeyWord(text):
     return[False]
 def Reply(event):
     Ktemp = KeyWord(event.message.text)
-	if Ktemp[0]:
+    if Ktemp[0]:
+        line_bot_api.reply_message(event.reply_token,
+           TextSendMessage(text = Ktemp[1]))
+    else:
 	    line_bot_api.reply_message(event.reply_token,
-	       TextSendMessage(text = Ktemp[1]))
-	 else:
-	    line_bot_api.reply_message(event.reply_token,
-	       TextSendMessage(text = event.message.text))
+           TextSendMessage(text = event.message.text))
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)

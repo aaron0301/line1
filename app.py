0723fc137030a5d3d0f26a4e0997beb540a8e183
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-06 09:50:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    a="123"
-    line_bot_api.reply_message(event.reply_token, a)
+    line_bot_api.reply_message(event.reply_token, message)
 	
-def Reply(text):
-	if text == "hi":
-		return '你好'
+def Reply(message):
+    if text == "hi":
+        return '你好'
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))

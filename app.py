f313fb12d67b3c15aaf1aec1bff3302c444ce921
aaron0301 <45398240+aaron0301@users.noreply.github.com>
2018-12-20 20:00:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,10 @@ def callback():
 ###========================================
 #關鍵字系統
 def Keyword(event):
-    KeyWordDict = {"你好":"我很好","你是誰":"才不告訴逆雷","故少":"臭ㄈㄓ","顧少":"臭ㄈㄓ","幹":"幹屁幹臭ㄈㄓ","靠北":"對 就是在靠北","臭":"你才臭"}
+    KeyWordDict = {"你好":["text","你也好啊"],
+                   "你是誰":["text","我是大帥哥"],
+                   "差不多了":["text","讚!!!"],
+                   "帥":["sticker",'1','120']}
 
     for k in KeyWordDict.keys():
         if event.message.text.find(k) != -1:
@@ -45,32 +48,33 @@ def Keyword(event):
             return True
     return False
 
-
 #按鈕版面系統
 def Button(event):
-    return TemplateSendMessage(
-        alt_text='特殊訊息，請進入手機查看',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://i.imgur.com/UWMxQ8g.jpg',
-            title='瑋桓是?',
-            text='快選擇阿',
-            actions=[
-                PostbackTemplateAction(
-                    label='bug',
-                    data='bug'
-                ),
-				MessageTemplateAction(
-                    label='87',
-                    text='87'               
-                ),
-				URITemplateAction(
-                    label='不知道只好google',
-                    uri='https://www.google.com/'
-                )
-               
-             ]
+    line_bot_api.reply_message(event.reply_token,
+        TemplateSendMessage(
+            alt_text='特殊訊息，請進入手機查看',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://github.com/54bp6cl6/LineBotClass/blob/master/logo.jpg?raw=true',
+                title='HPClub - Line Bot 教學',
+                text='大家學會了ㄇ',
+                actions=[
+                    PostbackTemplateAction(
+                        label='還沒',
+                        data='還沒'
+                    ),
+                    MessageTemplateAction(
+                        label='差不多了',
+                        text='差不多了'
+                    ),
+                    URITemplateAction(
+                        label='幫我們按個讚',
+                        uri='https://www.facebook.com/ShuHPclub'
+                    )
+                ]
+            )
         )
     )
+
 #指令系統，若觸發指令會回傳True
 def Command(event):
     tempText = event.message.text.split(",")
@@ -79,7 +83,7 @@ def Command(event):
         return True
     else:
         return False
-		
+
 #回覆函式，指令 > 關鍵字 > 按鈕
 def Reply(event):
     if not Command(event):
@@ -91,8 +95,10 @@ def Reply(event):
 def handle_message(event):
     try:
         Reply(event)
-        line_bot_api.push_message("Ub0778ded2c8eff813455c5a270089f46", TextSendMessage(text=event.source.user_id))
-        line_bot_api.push_message("Ub0778ded2c8eff813455c5a270089f46", TextSendMessage(text=event.message.text))
+        '''
+        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.source.user_id + "說:"))
+        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.message.text))
+        '''
     except Exception as e:
         line_bot_api.reply_message(event.reply_token, 
             TextSendMessage(text=str(e)))
@@ -101,18 +107,19 @@ def handle_message(event):
 @handler.add(PostbackEvent)
 def handle_postback(event):
     command = event.postback.data.split(',')
-    if command[0] == "bug":
+    if command[0] == "還沒":
         line_bot_api.reply_message(event.reply_token, 
-            TextSendMessage(text="你說的沒錯~~~"))
-       
+            TextSendMessage(text="還沒就趕快練習去~~~"))
+        
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         StickerSendMessage(
-            package_id=event.message.package_id,
-            sticker_id=event.message.sticker_id)
-    )	   
+            package_id='1',
+            sticker_id='410')
+    )
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
